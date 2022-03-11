2f05ee1c4ccdeb88c6947dc1eb4c4938acdc8780
luciomelis <luciomelis1992@gmail.com>
2022-03-11 19:24:24 +0100

    esercizio Jsnack2

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -6,3 +6,41 @@ Il programma stampa la somma di tutti i numeri inseriti.
 Esegui questo programma in due versioni, con il for e con il while.
 */
 
+
+// ESEMPIO FOR 
+/*
+
+let sommaTotale = 0;
+
+for (let i = 0; i < 5; i++) {
+    let numeroUtente;
+
+    while (isNaN(numeroUtente)) {
+        // creo una variabile con il prompt (richiesta all'utente)
+        numeroUtente = parseInt(prompt('Inserisci un numero'));
+    }
+
+    sommaTotale += numeroUtente;
+}
+
+console.log(sommaTotale);
+*/
+
+
+
+// ESEMPIO WHILE 
+
+let sommaTotale = 0;
+let i = 0;
+while (i < 5) {
+
+    let numeroUtente;
+
+    while (isNaN(numeroUtente)) {
+        numeroUtente = parseInt(prompt('Inserisci un numero'));
+    }
+    sommaTotale += numeroUtente;
+    i++
+}
+
+console.log(sommaTotale);
