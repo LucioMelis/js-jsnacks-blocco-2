66644a059a16e32fd6e43152d1a98a5210c6cd94
luciomelis <luciomelis1992@gmail.com>
2022-03-11 15:19:02 +0100

    jsnack1

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,1 +1,19 @@
 console.log('JS OK');
+/*
+Crea un array di numeri interi
+e fai la somma di tutti gli elementi che sono in posizione dispari.
+ */
+
+const array = [1, 2, 3, 4, 5];
+let somma = 0;
+
+
+for (let i = 0; i < array.length; i++) {
+
+    console.log(i);
+
+    if (i % 2 !== 0) {
+        somma += array[i];
+    }
+}
+console.log(somma);
